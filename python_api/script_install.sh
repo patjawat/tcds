fc751ba9043e1e849595aa270cabed400bb8c838
patjawat <patjawat@gmail.com>
2019-07-18 21:02:36 +0700

    เพิ่ม script install package

---
+++ python_api/script_install.sh
@@ -0,0 +1,7 @@
+#apt-get update 
+apt-get  install  apache2  php7.2 phpmyadmin mariadb-server \
+docker.io  docker-compose -y 
+apt install composer -y
+apt install php7.2-xml php7.2-mbstring -y
+apt-get install libzbar0 -y 
+apt install python3-pip -y  && pip3 install waitress jsonify flask flask_restful flask_cors pandas pyzbar Pillow opencv-python opencv-contrib-python opencv-python-headless opencv-contrib-python-headless simplejson

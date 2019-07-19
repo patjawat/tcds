d20b5748906ef62a3689b6560a9f64fb2eafd211
patjawat <patjawat@gmail.com>
2019-07-19 05:11:40 +0000

    mm

--- python_api/script_install.sh
+++ python_api/script_install.sh
@@ -1,6 +1,5 @@
-#apt-get update 
-apt-get  install  apache2  php7.2 phpmyadmin mariadb-server libapache2-mod-wsgi-py3 python-dev  \
-docker.io  docker-compose -y 
+apt-get update 
+apt-get  install  apache2  php7.2 phpmyadmin mariadb-server libapache2-mod-wsgi-py3 python-dev docker.io  docker-compose -y 
 apt install composer -y
 apt install php7.2-xml php7.2-mbstring -y
 apt-get install libzbar0 libapache2-mod-wsgi-py3 -y 

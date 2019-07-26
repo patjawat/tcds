3923a6797cedba18346f9a39d891409409b5008e
ปัจวัฒน์ ศรีบุญเรือง <patjawat@Macintosh.local>
2019-07-26 21:27:08 +0700

    แก้ API อ่าน QR-code

--- python_api/readDocQR.py
+++ python_api/readDocQR.py
@@ -11,62 +11,57 @@ import cv2
 import datetime
 from PIL import Image
 # import psycopg2
-import mysql.connector
-from mysql.connector import Error
-from mysql.connector import errorcode
+
 import simplejson as json
 import requests
 import json
 
-SOURCE_PATH = "../web/document-qr-him"
-DIST_PATH = "../web/document-qr"
+# SOURCE_PATH = "../web/document-qr-him"
+# DIST_PATH = "../web/document-qr"
+SOURCE_PATH = "../mount/ttr-scan-files"
+DIST_PATH = "../web/qr"
 root_dir = Path(SOURCE_PATH)
 
 
+def ReadQR(item):
+    img = cv2.imread(str(item))
+    barcodes = pyzbar.decode(img)
+    for barcode in barcodes:
+        (x, y, w, h) = barcode.rect
+        barcodeData = barcode.data.decode("utf-8")
+        barcodeType = barcode.type
+        if(barcodeType == 'QRCODE'):
+                if (barcodeData.split('-')[0]) == "tcds":
+                        hn = barcodeData.split('-')[1]
+                        vn = barcodeData.split('-')[2]
+                        docType = barcodeData.split('-')[3]
+                        page = barcodeData.split('-')[4]
+                        fileName = barcodeData+'.'+item.name.split('.')[1]
+                        print('tcds')
+                        # payload = {'file_name':fileName,'hn':hn,'vn':vn,'type_id':docType,'page':page}
+                        # r = requests.post("http://192.168.1.3:81/index.php?r=api/add-qr",json=payload)
+                        
+                        # if(r.text != 'null'):
+                        #         print('Scan '+fileName+' To database')
+                        #         moveFile(hn,data,dataText)
+                        # else:
+                        #         print('skip => '+fileName)
+                # else:
+                #         ''
 
-def moveFile(hn,data,dataText):
-        filename = dataText+'.'+data.name.split('.')[1]
-        # print(filename)
-  
-        # check directory
-        dir_path = DIST_PATH+'/'+hn
-        if not os.path.exists(dir_path):
-                os.makedirs(dir_path)
-        # End Check directory
-        shutil.copyfile(data,dir_path+'/'+filename)   #คัดลอกจากต้นฉบับมาที่ปลายทาง
-        os.remove(data)
-        return filename
-        
-
-def ReadQR(data):
-    image = cv2.imread(str(data))
-    decodedObjects = pyzbar.decode(image)
-    
-    for obj in decodedObjects:
-        dataText = obj.data.decode("utf-8")
-        dataType = obj.type
-        if(dataType == 'QRCODE'):
-                hn = dataText.split('-')[0]
-                vn = dataText.split('-')[1]
-                docType = dataText.split('-')[2]
-                page = dataText.split('-')[3]
-                # fileName = 
-                fileName = dataText+'.'+data.name.split('.')[1]
-                
-                payload = {'file_name':fileName,'hn':hn,'vn':vn,'type_id':docType,'page':page}
-                r = requests.post("http://192.168.1.3:81/index.php?r=api/add-qr",json=payload)
-                
-                if(r.text != 'null'):
-                        print('Scan '+fileName+' To database')
-                        moveFile(hn,data,dataText)
-                else:
-                        print('skip => '+fileName)
-        else:
-                ''
 
 if(root_dir.iterdir()):
-        for item in root_dir.iterdir():
-#                 #อ่าน QR-Code แล้วบันทึก
-                file = item.name
-                if os.path.splitext(file)[1] == ".jpg":
-                        ReadQR(item)
+    for item in root_dir.iterdir():
+        file = item.name
+        if os.path.splitext(file)[1] == ".jpg":
+                ReadQR(item)
+        
+        elif os.path.splitext(file)[1] == '.TIF':
+                ReadQR(item)
+        elif os.path.splitext(file)[1] == '.png':
+                ReadQR(item)
+        #     print(item.name)
+
+        # cv2.imwrite("new_img.jpg", img)
+        # barcodeData = barcode.data.decode("utf-8")
+        # barcodeType = barcode.type

5429f275ce49ba2d0a242aad39d3cd80e8f3f1c8
ปัจวัฒน์ ศรีบุญเรือง <patjawat@Macintosh.local>
2019-08-06 19:13:34 +0700

    edit dm indicator

--- modules/dmindicators/models/DmIndicators.php
+++ modules/dmindicators/models/DmIndicators.php
@@ -2,38 +2,37 @@
 
 namespace app\modules\dmindicators\models;
 
-use Yii;
-use yii\behaviors\TimestampBehavior;
-use yii\behaviors\BlameableBehavior;
-use yii\behaviors\AttributeBehavior;
-use yii\db\Expression;
-use yii\db\ActiveRecord;
 use app\components\DateTimeHelper;
-use app\components\PatientHelper;
-use yii\helpers\Json;
+use Yii;
+use yii\behaviors\AttributeBehavior;
+use yii\behaviors\BlameableBehavior;
+use yii\db\ActiveRecord;
 use yii\helpers\ArrayHelper;
 
 class DmIndicators extends \yii\db\ActiveRecord
 {
 
-  // Smoking Map
-  const SMOKING_NO = 1;
-  const SMOKING_YES = 2;
-  const SMOKING_ROLL_DAY = 3;
-  const SMOKING_EX = 4;
-  // Smoking Map End
+    // Smoking Map
+    const SMOKING_NO = 1;
+    const SMOKING_YES = 2;
+    const SMOKING_ROLL_DAY = 3;
+    const SMOKING_EX = 4;
+    // Smoking Map End
 
+    public static function getDb()
+    {
+        return Yii::$app->get('tcds');
+    }
 
     public static function tableName()
     {
         return 'dm_indicators';
     }
 
-
     public function rules()
     {
         return [
-            [['created_at', 'updated_at','eye_out_hos','neuropathy_vaseular','smoking','blindness','esrd','tds','flu_vaccine','foot_and_toe','macrovascular','target_of_control'], 'safe'],
+            [['created_at', 'updated_at', 'eye_out_hos', 'neuropathy_vaseular', 'smoking', 'blindness', 'esrd', 'tds', 'flu_vaccine', 'foot_and_toe', 'macrovascular', 'target_of_control'], 'safe'],
             [['eye_out_hos'], 'required'],
             // [['eye_out_hos'], 'string'],
             [['hn', 'vn', 'pcc_vn', 'created_by', 'updated_by'], 'string', 'max' => 255],
@@ -60,104 +59,106 @@ class DmIndicators extends \yii\db\ActiveRecord
         return [
             [
                 'class' => AttributeBehavior::className(),
-                'attributes' => [ActiveRecord::EVENT_BEFORE_INSERT => ['created_at','updated_at']],
-                'value' => DateTimeHelper::getDbNow()
-                
+                'attributes' => [ActiveRecord::EVENT_BEFORE_INSERT => ['created_at', 'updated_at']],
+                'value' => DateTimeHelper::getDbNow(),
+
             ],
             [
                 'class' => AttributeBehavior::className(),
                 'attributes' => [ActiveRecord::EVENT_BEFORE_UPDATE => 'updated_at'],
-                'value' => DateTimeHelper::getDbNow()            
-                
+                'value' => DateTimeHelper::getDbNow(),
+
             ],
             [
                 'class' => BlameableBehavior::className(),
-                'createdByAttribute'=>'created_by',
+                'createdByAttribute' => 'created_by',
                 'updatedByAttribute' => 'updated_by',
-            ]
-    
+            ],
+
         ];
     }
 
-
-
-    public static function itemsAlias($key){
+    public static function itemsAlias($key)
+    {
 
         $items = [
-          'smoking'=>[
-            1 => 'No',
-            2 => 'Yes',
-            3 => 'Roll / day',
-            4 => 'Ex'
-          ],
-          'HowLongAgo'=>[
-            1 => 'Unknown',
-            2 => '< 6 months',
-            3 => '6 months - 1.5 years',
-            4 => '1.5 - 2.5 years',
-            5 => '2.5 - 3.5 years',
-            6 => '3.5 - 5 years',
-            7 => '> 5 years',
-          ],
-          'FootAndToe'=>[
-            1 => 'Deformities',
-            2 => 'Ulcer',
-            3 => 'Amputation',
-          ],
-          'Macrovascular'=>[
-            1 => 'MI',
-            2 => 'Stroke',
-            3 => 'PAD',
-        ]
-      ];
-        return ArrayHelper::getValue($items,$key,[]);
+            'smoking' => [
+                1 => 'No',
+                2 => 'Yes',
+                3 => 'Roll / day',
+                4 => 'Ex',
+            ],
+            'HowLongAgo' => [
+                1 => 'Unknown',
+                2 => '< 6 months',
+                3 => '6 months - 1.5 years',
+                4 => '1.5 - 2.5 years',
+                5 => '2.5 - 3.5 years',
+                6 => '3.5 - 5 years',
+                7 => '> 5 years',
+            ],
+            'FootAndToe' => [
+                1 => 'Deformities',
+                2 => 'Ulcer',
+                3 => 'Amputation',
+            ],
+            'Macrovascular' => [
+                1 => 'MI',
+                2 => 'Stroke',
+                3 => 'PAD',
+            ],
+        ];
+        return ArrayHelper::getValue($items, $key, []);
         //return array_key_exists($key, $items) ? $items[$key] : [];
-      }
-
-
-      public function getItemSex()
-      {
-        return self::itemsAlias('sex');
-      }
-      public function getsmokingName(){
-        return ArrayHelper::getValue($this->getItemSmoking(),$this->smoking);
     }
-  
-      public function getItemSmoking()
-      {
-        return self::itemsAlias('smoking');
-      }
-  
-      public function getItemHowLongAgo()
-      {
-        return self::itemsAlias('HowLongAgo');
-      }
-  
-      public function getItemFootAndToe()
-      {
-        return self::itemsAlias('FootAndToe');
-      }
-  
-      public function getMacrovascular()
-      {
-        return self::itemsAlias('Macrovascular');
-      }
-  
-      public function getSexName(){
-          return ArrayHelper::getValue($this->getItemSex(),$this->sex);
-      }
-  
-      public function getMaritalName(){
-          return ArrayHelper::getValue($this->getItemMarital(),$this->marital_status);
-      }
-  
-      public function getEducationName(){
-          return ArrayHelper::getValue($this->getItemEducation(),$this->education_level);
-      }
-  
-      public function getTitleName(){
-          return ArrayHelper::getValue($this->getItemTitle(),$this->title);
-      }
 
- 
+    public function getItemSex()
+    {
+        return self::itemsAlias('sex');
+    }
+    public function getsmokingName()
+    {
+        return ArrayHelper::getValue($this->getItemSmoking(), $this->smoking);
+    }
+
+    public function getItemSmoking()
+    {
+        return self::itemsAlias('smoking');
+    }
+
+    public function getItemHowLongAgo()
+    {
+        return self::itemsAlias('HowLongAgo');
+    }
+
+    public function getItemFootAndToe()
+    {
+        return self::itemsAlias('FootAndToe');
+    }
+
+    public function getMacrovascular()
+    {
+        return self::itemsAlias('Macrovascular');
+    }
+
+    public function getSexName()
+    {
+        return ArrayHelper::getValue($this->getItemSex(), $this->sex);
+    }
+
+    public function getMaritalName()
+    {
+        return ArrayHelper::getValue($this->getItemMarital(), $this->marital_status);
+    }
+
+    public function getEducationName()
+    {
+        return ArrayHelper::getValue($this->getItemEducation(), $this->education_level);
+    }
+
+    public function getTitleName()
+    {
+        return ArrayHelper::getValue($this->getItemTitle(), $this->title);
+    }
+
 }

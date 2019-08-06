db5efe38887ffb08d7d140126eec95b9316d3696
ปัจวัฒน์ ศรีบุญเรือง <patjawat@Macintosh.local>
2019-08-06 19:24:30 +0700

    edit connect tcds

--- config/tcds_.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=mariadb;port=3306;dbname=tcds',
-    'username' => 'root',
-    'password' => 'docker',
-    'charset' => 'utf8',
-];

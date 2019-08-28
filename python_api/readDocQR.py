9ce3d7df2a563475c8712b17404dc0b53f2a7c80
patjawat <patjawat@gmail.com>
2019-08-28 15:56:12 +0700

    แก้ อ่านfile ที่เสียไม่ได้

--- python_api/readDocQR.py
+++ python_api/readDocQR.py
@@ -25,50 +25,57 @@ root_dir = Path(SOURCE_PATH)
 
 
 def ReadQR(item):
-    img = cv2.imread(str(item))
 
-    image = Image.open(item)
-    barcodes = pyzbar.decode(image)
-    for barcode in barcodes:
-        (x, y, w, h) = barcode.rect
-        barcodeData = barcode.data.decode("utf-8")
-        barcodeType = barcode.type
-        # print(item.name)
-        if(barcodeType == 'QRCODE'):
-            if (barcodeData.split('-')[0]) == "tcds":
-                hn = barcodeData.split('-')[1]
-                vn = barcodeData.split('-')[2]
-                docType = barcodeData.split('-')[3]
-                page = barcodeData.split('-')[4]
-                fileName = barcodeData+'.jpg'
-                dep = item.name.split('-_')[0]
-                # print(barcodeData)
+        try:
+            image = Image.open(item)
+            image.verify() # ถ้าไฟล์ใช้งานได้
+            # print('Valid image')
+            img = cv2.imread(str(item))
 
-                payload = {
-                    'file_name': fileName,
-                    'hn': hn,
-                    'vn': vn,
-                    'type_id': docType,
-                    'page': page,
-                    'dep': dep
-                }
-                r = requests.post(
-                    "http://10.1.88.8/tcds/web/index.php?r=api/add-qr", data=payload)
-                    # "http://127.0.0.1:81/index.php?r=api/add-qr", data=payload)
-                if(r.text != 'null'):
-                    dir_path = DIST_PATH+'/'+hn+'/'
-                    if not os.path.exists(dir_path):
-                        os.makedirs(dir_path)
+            image = Image.open(item)
+            barcodes = pyzbar.decode(image)
+            for barcode in barcodes:
+                (x, y, w, h) = barcode.rect
+                barcodeData = barcode.data.decode("utf-8")
+                barcodeType = barcode.type
+                # print(item.name)
+                if(barcodeType == 'QRCODE'): # ตรวจสอบ ประเภท QRCODE
+                    if (barcodeData.split('-')[0]) == "tcds": # ถ่าเป็นของ TCDS ให้แยกข้อมูลดังนี้
+                        hn = barcodeData.split('-')[1]
+                        vn = barcodeData.split('-')[2]
+                        docType = barcodeData.split('-')[3]
+                        page = barcodeData.split('-')[4]
+                        fileName = barcodeData+'.jpg'
+                        dep = item.name.split('-_')[0]
+                        # print(barcodeData)
 
-                    image.convert('L').save(dir_path+barcodeData+'.jpg')
-                    print('Scan '+fileName+' To database Success')
-                    # os.remove(item)
-                #     moveFile(hn, data, dataText)
-                 
-                else:
-                    print('skip => '+fileName)
-                    print(item)
-    
+                        payload = { # เก็ยตัวแปรเป็น JSON
+                            'file_name': fileName,
+                            'hn': hn,
+                            'vn': vn,
+                            'type_id': docType,
+                            'page': page,
+                            'dep': dep
+                        }
+                        # r = requests.post(
+                        #     "http://10.1.88.8/tcds/web/index.php?r=api/add-qr", data=payload)
+                        #     # "http://127.0.0.1:81/index.php?r=api/add-qr", data=payload)
+                        # if(r.text != 'null'): # ถ้ามีการบัยทึกฝั่ง server สำเร็จ ให้เก็บไฟล์
+                        #     dir_path = DIST_PATH+'/'+hn+'/'
+                        #     if not os.path.exists(dir_path):
+                        #         os.makedirs(dir_path)
+
+                        #     image.convert('L').save(dir_path+barcodeData+'.jpg')
+                        #     print('Scan '+fileName+' To database Success')
+                        #     os.remove(item)
+                        #     moveFile(hn, data, dataText)
+                        
+                        # else:
+                        #     print('skip => '+fileName)
+                            # print(item)
+
+        except Exception: # ถ้าช้ไม่ได้ให้ผ่านไป
+            print('Invalid image'+str(item))
 
 
 if(root_dir.iterdir()):
@@ -81,10 +88,9 @@ if(root_dir.iterdir()):
         elif os.path.splitext(file)[1] == '.tif':
             if file.split('-_')[0]:
                 ReadQR(item)
-        
+        # else:
+            
         os.remove(item)
-        #     if file.split('-_')[0]:
-        #         ReadQR(item)
 
         # cv2.imwrite("new_img.jpg", img)
         # barcodeData = barcode.data.decode("utf-8")
